509943c4bfadba12435d5fd5063386c9b1ab9905
massone99 <lorenzomassone@gmail.com>
2023-03-07 13:10:10 +0100

    Exercise 3

--- Esercizio 03/routes.js
+++ Esercizio 03/routes.js
@@ -4,6 +4,7 @@ const requestHandler = (req, res) => {
             res.write('<html lang="en">');
             res.write('<head><title>Enter Message</title></head>');
             res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Invia</button></form></body>');
+            res.end();
         }
     } else if (req.method === 'POST') {
         if (req.url === '/message') {
@@ -18,7 +19,7 @@ const requestHandler = (req, res) => {
                 res.write('<html lang="en">');
                 res.write('<body><h1>' + parsedBody.split('=')[1] + '</h1></body>');
                 res.write('</html>');
-                return res.end();
+                res.end();
             });
         }
     }
